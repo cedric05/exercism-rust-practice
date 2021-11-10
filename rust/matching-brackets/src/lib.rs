082e9becd9da9cc6b74d4b8823457709d8ea5ea2
prasanth <kesavarapu.siva@gmail.com>
2021-11-10 08:49:58 +0000

    test matching-brackets (2)

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -2,25 +2,18 @@ use std::collections::HashMap;
 
 pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack: Vec<char> = vec![];
-    let mut balanced = true;
     let char_map = HashMap::from([(')', '('), (']', '['), ('}', '{')]);
     for i in string.chars() {
         match i {
             '(' | '[' | '{' => stack.push(i),
-            ')' | '}' | ']' => {
-                let get = *char_map.get(&i).unwrap();
-                if let Some(has) = stack.pop() {
-                    if has != get {
-                        balanced = false;
-                        break;
-                    }
-                } else {
-                    balanced = false;
-                    break;
+            ')' | '}' | ']' => match stack.pop() {
+                Some(available) if available == *char_map.get(&i).unwrap() => {}
+                _ => {
+                    return false;
                 }
-            }
+            },
             _ => {}
         }
     }
-    stack.is_empty() && balanced
+    stack.is_empty()
 }
